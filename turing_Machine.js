830242ca30f7715a7a9b9296207cf3e420f27db6
BrunoRRamos <brbosshd2@gmail.com>
2022-11-24 18:58:04 -0300

    funcionandO

--- turing_Machine.js
+++ turing_Machine.js
@@ -1,7 +1,8 @@
 import chalk from "chalk";
+import { exit } from "process";
 
 function setStop(arg) {
-    if (arg === true) {
+    if (arg) {
         return true;
     }
     return false;
@@ -16,9 +17,25 @@ function stopped(argB) {
 
 function s(argS) {
     if(stopped(argS)) {
-        return false;
+        return true;
     }
-    return true;
+    return false;
+}
+
+function criaParadoxo(boolean) {
+    console.log(chalk.magenta("Criando paradoxo:"))
+    console.log(chalk.blackBright(chalk.cyan("Entrada fornecida"), chalk.white(boolean), "\n"));
+    
+    stopped(boolean) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"));
+
+    if(s(s(boolean)) === false) {
+        s(boolean) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
+        console.log(chalk.yellow("Encerra"))
+        exit(0);
+    }
+
+    s(boolean) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
+    console.log(chalk.yellow("Quebrou"))
 }
 
 console.log(chalk.yellow("Se stopped for true o programa encerra."))
@@ -27,4 +44,6 @@ s(true) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("fals
 
 console.log(chalk.yellow("Se stopped for false o programa entra em loop."))
 stopped(false) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
-s(false) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
+s(false) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("false\n"))
+
+criaParadoxo(false)
