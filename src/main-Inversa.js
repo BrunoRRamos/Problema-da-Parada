0b881aa6b332996d6291a7c93666000839320fd4
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-02 19:06:20 -0300

    fix

--- src/main-Inversa.js
+++ src/main-Inversa.js
@@ -8,7 +8,7 @@ function setStop(arg) {
     return false;
 }
 
-//Iforma a parada da máquina
+//Informa a parada da máquina
 function stopped(argB) {
     if (setStop(argB)) {
         return true;
@@ -16,7 +16,7 @@ function stopped(argB) {
     return false;
 }
 
-//Inverte a função "stopped": informa quando entra em loop.
+//Inverte a função "stopped": informa quando NÃO entra em loop.
 function s(argS) {
     if(stopped(argS)) {
         return true;
@@ -40,15 +40,6 @@ function criaParadoxo(boolean) {
     console.log(chalk.yellow("Entrou em loop"))
 }
 
-console.log(chalk.yellow("Se stopped for true o programa encerra."))
-stopped(true) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
-s(true) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("false\n"))
-
-console.log(chalk.yellow("Se stopped for false o programa entra em loop."))
-stopped(false) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
-s(false) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("false\n"))
-
 let arg = process.argv;
 arg[2] === "true" ? arg = true : arg = false;
 criaParadoxo(arg)
-
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import chalk from "chalk";
-import { exit } from "process";
 
 function setStop(arg) {
     if (arg) {
@@ -8,7 +7,7 @@ function setStop(arg) {
     return false;
 }
 
-//Iforma a parada da máquina
+//Informa a parada da máquina
 function stopped(argB) {
     if (setStop(argB)) {
         return true;
@@ -16,7 +15,7 @@ function stopped(argB) {
     return false;
 }
 
-//Inverte a função "stopped": informa quando entra em loop.
+//Inverte a função "stopped": retorna se entrou em loop.
 function s(argS) {
     if(stopped(argS)) {
         return false;
@@ -24,11 +23,15 @@ function s(argS) {
     return true;
 }
 
-
+//Entrada: true
+console.log(chalk.cyan("Entrada:", chalk.green("true\n")))
 console.log(chalk.yellow("Se stopped for true o programa encerra."))
-stopped(true) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
-s(true) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("false\n"))
+stopped(true) ? console.log(chalk.magenta("Stopped:"), chalk.green("true")) : console.log(chalk.magenta("Stopped:"), chalk.redBright("false"))
+s(true) ? console.log(chalk.magenta("s:"), chalk.green("true\n")) : console.log(chalk.magenta("s:"), chalk.redBright("false\n"))
+console.log(chalk.gray("----------------------------------------------\n"))
 
+//Entrada: false
+console.log(chalk.cyan("Entrada:", chalk.redBright("false\n")))
 console.log(chalk.yellow("Se stopped for false o programa entra em loop."))
-stopped(false) ? console.log(chalk.green("true")) : console.log(chalk.redBright("false"))
-s(false) ? console.log(chalk.green("true\n")) : console.log(chalk.redBright("false\n"))
+stopped(false) ? console.log(chalk.magenta("Stopped: "), chalk.green("true")) : console.log(chalk.magenta("Stopped:"), chalk.redBright("false"))
+s(false) ? console.log(chalk.magenta("s:"), chalk.green("true\n")) : console.log(chalk.magenta("s:"), chalk.redBright("false\n"))
